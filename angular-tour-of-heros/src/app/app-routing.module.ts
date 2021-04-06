b2b7cd9f1cac7a75c882bdad79e91641331ea78b
Douglas-Felipe <douglasfelipe-8@hotmail.com>
2021-04-06 13:00:25 -0300

    Adicionando um /edit
    Adicionando uma nova pagina /edit e atualizando a lista de herois

--- angular-tour-of-heros/src/app/app-routing.module.ts
+++ angular-tour-of-heros/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroEditComponent } from './hero-edit/hero-edit.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'edit/:id', component:HeroEditComponent}
 ];
 
 @NgModule({
