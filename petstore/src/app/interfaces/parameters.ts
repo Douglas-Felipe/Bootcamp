3f868c165428ca23959042e9b4f85534e41c8a6d
Douglas-Felipe <douglasfelipe-8@hotmail.com>
2021-04-12 13:33:48 -0300

    Definindo a url da API e criando as interfaces principais do projeto

---
+++ petstore/src/app/interfaces/parameters.ts
@@ -0,0 +1,23 @@
+export interface Parameters {
+    company_name: string;
+    trademark: string;
+    email: string;
+    address: string;
+    address_complement: string;
+    city: string;
+    state: string;
+    zip_code: string;
+    social_networks: Array<social_networks>;
+    phones: Array<phones>;
+}
+
+export interface social_networks{
+    account: string;
+    name: string;
+}
+
+export interface phones {
+    number: string;
+    type: string;
+    country_code: string;
+}
